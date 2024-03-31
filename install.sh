4e0298ccc35bf7cecf8c842f4e01f6a8055c4337
Machillka <2437844684@qq.com>
2024-04-01 01:17:07 +0800

    update

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ cat << EOF
 Usage: $0 [OPTION]...
 
 OPTIONS:
-  -t, --theme     theme variant(s)          [tela|vimix|stylish|whitesur]       (default is tela)
+  -t, --theme     theme variant(s)
   -i, --icon      icon variant(s)           [color|white|whitesur]              (default is color)
   -s, --screen    screen display variant(s) [1080p|2k|4k|ultrawide|ultrawide2k] (default is 1080p)
   -r, --remove    Remove theme              [tela|vimix|stylish|whitesur]       (must add theme name option, default is tela)
---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+IMG_READ_PATH="customizer/background/"
+MOD_IMG_PATH="backgrounds/"
+BACKGROUND_OPTION=("2k" "4k" "1080p" "ultrawide" "ultrawide2k")
+
+# ModifyBackGround() {
+
+# Customizer background
+imgFiles=$(ls $IMG_READ_PATH)
+
+if [ -z "${imgFiles[*]}" ]; then
+    echo "No background Found"
+else
+    for file in ${imgFiles[@]}; do
+        if [ ${file##*.} = "png" ] || [ ${file##*.} = "jpg" ] || [ ${file##*.} = "jpge" ]; then
+            echo "Found background" $file
+            read -p "Confirm (y/n)?" choice
+            case $choice in
+                [Nn][Oo][Nn])
+                    echo "Continue without customizering the wallpaper"
+                    ;;
+                [yY][eE][sS][yY][])
+                    echo "Customizing your background"
+                    for folder in ${BACKGROUND_OPTION[@]}; do
+                        cp -f $IMG_READ_PATH$file $MOD_IMG_PATH$folder"/background-tela.jpg"
+                    done
+                    ;;
+            esac
+
+            break
+        fi
+    done
+fi
+
+sudo ./install.sh $@
